27803308c4c619794574c25cab0ef9e5292c0b08
Melina <melina.gilsing@students.unibe.ch>
2022-12-18 20:58:00 +0100

    Last Update

--- Predicting_future_values_V2.R
+++ Predicting_future_values_V2.R
@@ -20,7 +20,7 @@ autoplot(fit_arima_2, include = 2500)
 fit_ets_1 <- forecast(x_cgm, method=c("ets"), h = 240)
 autoplot(fit_ets_1, include = 2500)
 
-fit_ets_3 <- forecast(x_cgm, method=c("ets"), h = 1440)
+fit_ets_2 <- forecast(x_cgm, method=c("ets"), h = 1440)
 autoplot(fit_ets_2, include = 2500)
 
 
@@ -30,13 +30,5 @@ autoplot(fit_naive_1, include = 2500)
 
 fit_naive_2 <- forecast(x_cgm, method=c("naive"), h = 1440)
 autoplot(fit_naive_2, include = 2500)
-
-
-# Method 4 - Random Walk with drift
-fit_rwdrift_1 <- stlf(x_cgm, method=c("rwdrift"), h = 240)
-autoplot(fit_rwdrift_1, include = 2500)
-
-fit_rwdrift_2 <- stlf(x_cgm, method=c("rwdrift"), h = 1440)
-autoplot(fit_rwdrift_2, include = 2500)
 
 
